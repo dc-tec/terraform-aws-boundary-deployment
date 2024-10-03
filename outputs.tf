07f3c0f651db470207815da2bf4c16a7036e5d36
Roel de Cort <roel@decort.tech>
2024-10-04 00:25:33 +0200

    add descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,18 @@
 output "boundary_worker_auth_key" {
+  description = "Boundary Worker Authentication Key, used for initializition of Boundary"
+
   value     = aws_kms_key.boundary_worker_auth.id
   sensitive = true
 }
 
 output "boundary_lb_dns" {
+  description = "DNS address of the Load Balancer, if chosen to opt out of route53 usage"
+
   value = aws_lb.boundary_lb.dns_name
 }
 
 output "boundary_worker_sg_id" {
+  description = "Boundary Worker Security Group ID"
+
   value = aws_security_group.boundary_worker.id
-}
-
-output "boundary_aws_plugin_access_key_id" {
-  value = aws_iam_access_key.boundary.id
-}
-
-output "boundary_aws_plugin_secret_access_key" {
-  value     = aws_iam_access_key.boundary.secret
-  sensitive = true
 }
